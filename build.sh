8569ab40244735c716aca6c2306238ae2adcae18
hamzarafiq02 <hamzarafiq9876@gmail.com>
2024-06-05 21:45:52 +0500

    update vercel

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -o errexit  # exit when there is an error error
 
-pip install -r requirements.txt
+python3.10 -m pip install -r requirements.txt
 
 python3.10 manage.py collectstatic --noinput --clear
 
